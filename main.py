ddeec4105b24c8fb235eac074b3308dba361b538
Emmanuel Munoz <emunozce80@gmail.com>
2024-06-16 17:25:28 -0600

    chore: Add resend library for email notifications

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import time
 import requests
 import serial
 import boto3
-
-# Initialize the DynamoDB client
-dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table("monitoreo_ambiental")
+import resend
 
 # Open the serial port
 ser = serial.Serial("COM6", 9600)  # Replace 'COM3' with your Arduino's serial port
@@ -20,10 +17,16 @@ def read_from_serial():
         while True:
             if ser.in_waiting > 0:
                 # Read data from serial
-                ldr_value = ser.readline().decode("utf-8").strip()
-                temperature = ser.readline().decode("utf-8").strip()
-                humidity = ser.readline().decode("utf-8").strip()
-                soil_moisture = ser.readline().decode("utf-8").strip()
+                data = ser.readline().decode("utf-8").strip()
+
+                # Split the data into its parts
+                temperature, humidity, soil_moisture, ldr_value = data.split(",")
+
+                # Print the received data
+                print(f"Temperature: {temperature}°C")
+                print(f"Humidity: {humidity}%")
+                print(f"LDR Value: {ldr_value}")
+                print(f"Soil Moisture: {soil_moisture}")
 
                 # Print the received data
                 print(f"LDR Value: {ldr_value}")
@@ -43,6 +46,16 @@ def read_from_serial():
                 print(f"Response Status Code: {response.status_code}")
                 print(f"Response Body: {response.text}")
 
+                resend.api_key = "re_Y2bSb3Ya_PAByFhtBrJqpw1bEHSqY2Lgk"
+
+                r = resend.Emails.send(
+                    {
+                        "from": "onboarding@resend.dev",
+                        "to": "pablo.martinez@edu.uaa.mx",
+                        "subject": "Monitor ambiental inteligente",
+                        "html": "<p>Checa los datos de tu planta</p><a>https://plant-proyect.onrender.com</a>",
+                    }
+                )
                 # Add any additional processing of the data here
 
                 # Delay for a bit before reading the next set of data
